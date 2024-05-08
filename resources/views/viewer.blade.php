a33b9c4843efe5395233e9606b07279832835f0f
nikolozi <nikolozi0525@gmail.com>
2024-05-08 13:54:35 +0400

    Update frontend

--- resources/views/viewer.blade.php
+++ resources/views/viewer.blade.php
@@ -6,11 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1"/>
     <script type="text/javascript" src="{{ 'js/viewer.js' }}"></script>
+    <link rel="stylesheet" href="{{ 'css/custom.css' }}">
 </head>
 <body style="margin: 0px">
-    <label for="file-picker"> Choose a PDF file:</label>
-    <input type="file" id="file-picker" accept="application/pdf">
-    <div id="adobe-dc-view"></div>
+    <div id="adobe-dc-view" class="pdf-view"></div>
+    <div class="custom-container d-center">
+        <input type="file" id="file-picker" accept=".pdf,.epub"  style="display: none;">
+        <input type="button" value="Browse..." onclick="document.getElementById('file-picker').click();" />
+    </div>
+    
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://acrobatservices.adobe.com/view-sdk/viewer.js"></script>
 </body>
 </html>
