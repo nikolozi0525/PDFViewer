b80100956eaa06cfcba776a6ee62c5f6a73f9120
nikolozi <nikolozi0525@gmail.com>
2024-05-08 22:06:06 +0400

    bug fix converter

--- convert-server.js
+++ convert-server.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "temp/");
+        cb(null, __dirname + "/temp/");
     },
     filename: (req, file, cb) => {
         cb(null, "temp.epub");
