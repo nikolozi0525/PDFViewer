8ff5c351a3168d0357a0d567b7321e3dbdfe971e
nikolozi <nikolozi0525@gmail.com>
2024-05-08 13:55:09 +0400

    Add epub file view feature

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -12,8 +12,15 @@ written permission of Adobe.
 /* Control the default view mode */
 const viewerConfig = {
     /* Allowed possible values are "FIT_PAGE", "FIT_WIDTH", "TWO_COLUMN", "TWO_COLUMN_FIT_PAGE" or "". */
-    defaultViewMode: "SIZED_CONTAINER",
-    enableLinearization: true,
+    defaultViewMode: "FIT_WIDTH",
+    embedMode: "FULL_WINDOW",
+    showDownloadPDF: false,
+    showPrintPDF: false,
+    showLeftHandPanel: true,
+    showBookmarks: true,
+    // showDisabledSaveButton: true,
+
+    // enableLinearization: true,
 };
 
 /* Wait for Adobe Acrobat Services PDF Embed API to be ready */
@@ -23,26 +30,54 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
     fileToRead.addEventListener(
         "change",
         function (event) {
+            /* Initialize the AdobeDC View object */
+            const adobeDCView = new AdobeDC.View({
+                /* Pass your registered client id */
+                clientId: "ef8f6d21909d4d0189accb2e78167985",
+                /* Pass the div id in which PDF should be rendered */
+                divId: "adobe-dc-view",
+            });
+
             var files = fileToRead.files;
             if (files.length > 0) {
-                var reader = new FileReader();
-                reader.onloadend = function (e) {
-                    const filePromise = Promise.resolve(e.target.result);
-                    /* Initialize the AdobeDC View object */
-                    const adobeDCView = new AdobeDC.View({
-                        /* Pass your registered client id */
-                        clientId: "ef8f6d21909d4d0189accb2e78167985",
-                        /* Pass the div id in which PDF should be rendered */
-                        divId: "adobe-dc-view",
-                    });
+                if (files[0].type === "application/pdf") {
+                    var reader = new FileReader();
+                    reader.onloadend = function (e) {
+                        const filePromise = Promise.resolve(e.target.result);
 
-                    // Pass the filePromise and name of the file to the previewFile API
-                    adobeDCView.previewFile({
-                        content: { promise: filePromise },
-                        metaData: { fileName: files[0].name },
+                        // Pass the filePromise and name of the file to the previewFile API
+                        adobeDCView.previewFile(
+                            {
+                                content: { promise: filePromise },
+                                metaData: { fileName: files[0].name },
+                            },
+                            viewerConfig
+                        );
+                    };
+                    reader.readAsArrayBuffer(files[0]);
+                } else {
+                    const formData = new FormData();
+                    formData.append("epub", files[0]);
+                    $.ajax({
+                        url: "http://localhost:4464/epub2pdf",
+                        type: "POST",
+                        data: formData,
+                        success: function (data) {
+                            adobeDCView.previewFile(
+                                {
+                                    content: {
+                                        location: { url: "temp/temp.pdf" },
+                                    },
+                                    metaData: { fileName: files[0].name },
+                                },
+                                viewerConfig
+                            );
+                        },
+                        cache: false,
+                        contentType: false,
+                        processData: false,
                     });
-                };
-                reader.readAsArrayBuffer(files[0]);
+                }
             }
         },
         false
